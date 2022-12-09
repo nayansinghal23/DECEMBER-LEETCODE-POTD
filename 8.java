7c891db08247f7f334e8cf7188587d2d01cacffc
Nayan Singhal <96867955+nayansinghal23@users.noreply.github.com>
2022-12-09 09:30:43 +0530

    Important Question. I need to revise its concept

---
+++ 8.java
@@ -0,0 +1,21 @@
+// TIME COMPLEXITY : O(N)
+// SPACE COMPLEXITY : O(N)
+
+/*
+ class Solution {
+    private int dfs(TreeNode node, int min, int max) {
+        if (node == null)
+            return Integer.MIN_VALUE;
+
+        min = Math.min(min, node.val);
+        max = Math.max(max, node.val);
+
+        int leftMax = dfs(node.left, min, max);
+        int rightMax = dfs(node.right, min, max);
+        return Math.max(max - min, Math.max(leftMax, rightMax));
+    }
+    public int maxAncestorDiff(TreeNode root) {
+        return dfs(root, root.val, root.val);
+    }
+}
+ */

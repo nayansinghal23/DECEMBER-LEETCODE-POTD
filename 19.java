ea457d611073f64fc0a69cebc6a00050b62ab00e
Nayan Singhal <96867955+nayansinghal23@users.noreply.github.com>
2022-12-20 09:25:17 +0530

    Add files via upload

---
+++ 19.java
@@ -0,0 +1,24 @@
+// TIME COMPLEXITY : O(N)
+// SPACE COMPLEXITY : O(N) + O(N) [AUXILIARY STACK SPACE + VISITED ARRAY]
+
+/*
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        if(rooms.size() == 0) return true;
+        boolean[] visited = new boolean[rooms.size()];
+        dfs(0,rooms,visited);
+        for(int i=0;i<rooms.size();i++)
+            if(!visited[i])
+                return false;
+        return true;
+    }
+    public void dfs(int index,List<List<Integer>> rooms,boolean[] visited){
+        visited[index] = true;
+        for(int n:rooms.get(index)){
+            if(!visited[n]){
+                dfs(n,rooms,visited);
+            }
+        }
+    }
+}
+ */
